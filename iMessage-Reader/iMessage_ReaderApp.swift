b9fc8d658c775205b89a790462e4ac1a2c752d4a
David Zorychta <macmee@gmail.com>
2022-10-06 17:14:38 -0700

    Initial Commit

---
+++ iMessage-Reader/iMessage_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessage_ReaderApp.swift
+//  iMessage-Reader
+//
+//  Created by David Zorychta on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct iMessage_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
